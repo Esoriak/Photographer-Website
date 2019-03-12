3a0d6b1bf1ddaec0b7c23dcc007830c75c878016
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-03-12 12:30:19 +0100

    [Update] Fade-in animation and added contdition to prevent constant new quote rendering on scroll

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -6,30 +6,48 @@ document.addEventListener("DOMContentLoaded", function () {
     const navQuote = document.querySelector("#quote");
 
     // Quotes variables
-    const landingQuote = "La création est la naissance de quelque chose..."
-    const eventQuote = "Depasser le superficiel pour accéder à l'essentiel";
-    const galleryQuote = "Les galleries d'images sont overrated"
+    const landingQuote = "<span>L</span>a création est la naissance de quelque chose. . ."
+    const eventQuote = "<span>D</span>epasser le superficiel pour accéder à l'essentiel";
+    const galleryQuote = "<span>L</span>es galleries d'images sont overrated"
 
+
+    // Checks scroll position function when user scroll
     window.onscroll = getScrollPosition;
 
     function changeQuote(quote) {
-        navQuote.textContent = quote;
+
+        // Change HTML of navbar quote
+        navQuote.innerHTML = quote;
+
+        // Fade in nimation on quote change
+        navQuote.animate([
+            { opacity: 0},
+            { opacity: 1}
+        ], {
+            duration: 1500,
+            easing: "ease-in"
+        })
+        
     }
-    
+
     function getScrollPosition(e) {
-        // Do something with scroll position
+
+        // Store scroll position
         scrollObject = {
             x: window.pageXOffset,
             y: window.pageYOffset
         }
 
-        // Check if current scroll position is in Event Section
-        if (scrollObject.y < eventSection.offsetTop) {
+        //Testing
+        console.log(gallerySection.offsetTop);
+        
+        // // Compares current scroll Y position to the different sections and change the quote accordingly
+        if (scrollObject.y < eventSection.offsetTop - 80 && navQuote.innerHTML !== landingQuote) {
             changeQuote(landingQuote);
-        } else if (scrollObject.y >= gallerySection.offsetTop) {
-            changeQuote(galleryQuote);
-        } else if(scrollObject.y >= eventSection.offsetTop) {
+        } else if (scrollObject.y >= eventSection.offsetTop - 80 && scrollObject.y < gallerySection.offsetTop - 80 && navQuote.innerHTML !== eventQuote) {
             changeQuote(eventQuote);
+        } else if (scrollObject.y >= gallerySection.offsetTop - 80 && navQuote.innerHTML !== galleryQuote) {
+            changeQuote(galleryQuote);
         }
     }
 
