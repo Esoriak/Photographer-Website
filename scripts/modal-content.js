7e56ff475878d084368ba7db607068522585c7cd
helderscrolls <nortehelder@gmail.com>
2019-03-19 17:51:30 +0100

    added modal-content.js - it dynamically adds content inside the modals.

--- scripts/modal-content.js
+++ scripts/modal-content.js
@@ -1,8 +1,8 @@
 const modals = [
-	{ buttonId: "#button1", modalId: '#projet1', src: '', text: 'test texte 1', titleOne: 'Douceur Animale', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
-	{ buttonId: "#button2", modalId: '#projet2', src: '', text: 'test texte 2', titleOne: 'Infinite Loop', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
-	{ buttonId: "#button3", modalId: '#projet3', src: '', text: 'test texte 3', titleOne: 'Inception Varsovienne', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
-	{ buttonId: "#button4", modalId: '#projet4', src: '', text: 'test texte 4', titleOne: 'Marseille, a la bien cousing !', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
+	{ buttonId: "#button1", modalId: '#projet1', src: 'images/animals-basket.jpg', text: 'test texte 1', titleOne: 'Douceur Animale', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
+	{ buttonId: "#button2", modalId: '#projet2', src: 'images/eagle-eye.jpg', text: 'test texte 2', titleOne: 'Infinite Loop', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
+	{ buttonId: "#button3", modalId: '#projet3', src: 'images/architecture-building.jpg', text: 'test texte 3', titleOne: 'Inception Varsovienne', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
+	{ buttonId: "#button4", modalId: '#projet4', src: 'images/aerial-beach.jpg', text: 'test texte 4', titleOne: 'Marseille, a la bien cousing !', titleTwo: 'Project Description', titleThree: 'Project Tips/Details', liste: '' },
 ];
 
 modals.forEach(modalObject => {
@@ -17,5 +17,5 @@ function ajaxSuccess(modalObject) {
 	modal.find('.title-test-two').html(modalObject.titleTwo);
 	modal.find('.title-test-three').html(modalObject.titleThree);
 	modal.find('.modals-text-1').html(modalObject.text);
-	modal.find('modal-image-1').html(modalObject.src);
+	modal.find('.modal-image-1').attr('src', modalObject.src);
 }
